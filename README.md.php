b86a2f4ea93fe634de648f11d4541e25ddb2c231
Kay Stenschke <info@stenschke.com>
2015-02-03 11:22:19 +0100

    added readme

---
+++ README.md.php
@@ -0,0 +1,36 @@
+Header Theme for MantisBT
+=========================
+
+Minimal plugin to adapt the logo image displayed in the header of MantisBT
+
+
+Compatibility
+-------------
+
+The theme was created and tested with Mantis release 1.2.17
+
+
+Installation
+------------
+
+1. Copy the "MantisHeaderTheme" folder into the "plugins" directory of your MantisBT installation
+2. Activate the plugin in the "Manage > Plugins" section of the MantisBT backend
+
+
+How to use your own logo image
+------------------------------
+
+The plugin installs two new logo images to replace the default image displayed in the header.
+1. plugins/MantisHeaderTheme/files/images/logo-header.png
+2. plugins/MantisHeaderTheme/files/images/logo-header-login.png
+
+To use your own logo images in the header, simply replace the above image files with your own.
+Image 1 will be displayed in the header of all pages after login, image 2 is shown in the login page only.
+
+
+Authors and License
+-------------------
+
+Created in 2015 by Kay Stenschke.
+
+Licensed under the GNU GENERAL PUBLIC LICENSE, Version 2.
